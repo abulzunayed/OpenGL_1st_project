984a744b92ef8313e7dd328e0c978b87f5926818
wo65beja <abul.km.zunayed@fau.de>
2024-07-10 00:26:55 +0200

    openGl

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/C++ project3/OpenGL/OpenGL_1st_project
+# Build directory: E:/C++ project3/OpenGL/OpenGL_1st_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
